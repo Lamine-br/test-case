908906e6416cc7fc35fd13d21ff4a8e8a3f6f0e2
Lamine-br <jl_brahami@esi.dz>
2024-12-15 15:28:01 +0100

    Step 1 - add a description

--- apps/api/src/db/schemas/course-schema.js
+++ apps/api/src/db/schemas/course-schema.js
@@ -5,18 +5,22 @@ const questionSchema = require("./question-schema");
 const Schema = mongoose.Schema;
 
 const courseSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    questions: {
-        type: [mongoose.Schema.Types.ObjectId],
-        ref: "Question",
-        default: [],
-    },
-  },
-  { timestamps: true }
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			maxlength: 256,
+		},
+		questions: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: "Question",
+			default: [],
+		},
+	},
+	{ timestamps: true }
 );
 
 module.exports = courseSchema;
--- apps/web/src/models/course.model.ts
+++ apps/web/src/models/course.model.ts
@@ -1,5 +1,6 @@
 export type Course = {
   _id: string;
   title: string;
+  description: string;
   questions: unknown;
 };

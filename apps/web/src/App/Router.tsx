ae59ce9ff1f77502bf472b97336013a32b60565c
Lamine-br <jl_brahami@esi.dz>
2024-12-15 16:46:07 +0100

    Step 5 - course detail page

--- apps/web/src/App/Router.tsx
+++ apps/web/src/App/Router.tsx
@@ -1,14 +1,22 @@
-import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from "react-router-dom";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Navigate,
+	Route,
+} from "react-router-dom";
 
 import App from "./App.tsx";
 import { CourseList } from "./courses/CourseList.tsx";
+import { CourseDetails } from "./courses/CourseDetails.tsx";
 
 export const Router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<App />} path="/">
-      <Route element={<CourseList />} path="courses" />
+	createRoutesFromElements(
+		<Route element={<App />} path='/'>
+			<Route element={<CourseList />} path='courses' />
 
-      <Route element={<Navigate replace={true} to='/courses' />} path='/' />
-    </Route>
-  )
+			<Route element={<CourseDetails />} path='courses/:code' />
+
+			<Route element={<Navigate replace={true} to='/courses' />} path='/' />
+		</Route>
+	)
 );
--- apps/web/src/App/courses/CourseDetails.tsx
+++ apps/web/src/App/courses/CourseDetails.tsx
@@ -1,5 +1,3 @@
 export const CourseDetails = () => {
-  return (
-    <>Course details</>
-  );
+	return <>Course details</>;
 };
